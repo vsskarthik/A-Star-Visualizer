1d2ace0bf3cb98cf352ee3e422c286c98929fd02
VSS Karthik(linux) <vsskarthik8@gmail.com>
2020-07-14 09:25:44 +0530

    generateGrid Function added

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,40 +2,68 @@
 #include <vector>
 #include<iostream>
 
+using namespace std;
+
 class Block
 {
  private:
    int Fcost, Hcost, Gcost;
+   char property;;
  public:
-    char property;
     sf::RectangleShape rect;
+
+    bool isSource(){
+      return property == 's';
+    }
+    bool isDestination(){
+      return property == 'd';
+    }
+    bool isObstacle(){
+      return property == 'o';
+    }
+    bool isBlank(){
+      return property == 'b';
+    }
+    void setProp(char p){
+      property = p;
+    }
+
+
     Block(int x,int y, int w,int h){
         rect.setPosition(x,y);
         rect.setSize(sf::Vector2f(w,h));
     }
     Block(){}
-
 };
+
+
+vector<vector<Block>> generateGrid(){
+  // Create Grid
+  int rows = 100;
+  int columns = 100;
+  vector<vector<Block>> grid(rows,vector<Block>(columns));
+  sf::Vector2f cellSize(15.0f, 15.0f);
+  for(int i=0;i<columns;i++){
+          for(int j=0;j<rows;j++){
+              grid[i][j].rect.setSize(cellSize);
+              grid[i][j].rect.setOutlineColor(sf::Color::Black);
+              grid[i][j].rect.setOutlineThickness(1.0f);
+              grid[i][j].rect.setPosition(i*cellSize.x + 5.0f, j*cellSize.y + 5.0f);
+              grid[i][j].setProp('b');
+          }
+  }
+  return grid;
+  // End Create Grid
+}
 
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(900,700), "A* Visualizer");
-    // Create Grid
     char mode = 's';
     int rows = 100;
     int columns = 100;
-    sf::Vector2f cellSize(15.0f, 15.0f);
-    Block grid[rows][columns];
-    for(int i=0;i<columns;i++){
-            for(int j=0;j<rows;j++){
-                grid[i][j].rect.setSize(cellSize);
-                grid[i][j].rect.setOutlineColor(sf::Color::Black);
-                grid[i][j].rect.setOutlineThickness(1.0f);
-
-                grid[i][j].rect.setPosition(i*cellSize.x + 5.0f, j*cellSize.y + 5.0f);
-            }
-    }
+    auto grid = generateGrid();
     while (window.isOpen())
     {
         sf::Event event;
@@ -52,14 +80,17 @@ int main()
                         if(rect.contains(localPosition.x,localPosition.y)){
                             if (mode == 'd'){
                                 grid[i][j].rect.setFillColor(sf::Color::Red);
+                                grid[i][j].setProp('d');
                                 mode = 'o';
                             }
                             else if(mode == 's'){
                                 grid[i][j].rect.setFillColor(sf::Color::Blue);
+                                grid[i][j].setProp('s');
                                 mode = 'd';
                             }
                             else if(mode == 'o'){
                                 grid[i][j].rect.setFillColor(sf::Color::Black);
+                                grid[i][j].setProp('o');
                             }
                         }
                 }
